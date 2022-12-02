c3d367647cda4d7e07eab4392f1529958fb4aecb
Katia <puzynia.katia@gmail.com>
2022-12-02 03:22:13 +0300

    первая задача

---
+++ Task_1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+using static System.Console;
+
+void FillArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100,1000);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Write($"{arr[i]} ");
+    }
+}
+
+int FindEven (int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+WriteLine("Введите целое натуральное число, определяющее размер массива: ");
+int n = Convert.ToInt32(ReadLine());
+int[] array = new int[n];
+
+PrintArray (array);
+WriteLine();
+FillArray (array);
+PrintArray (array);
+WriteLine();
+WriteLine(FindEven(array));
